8fe99e782fc019fd6c5be00256e02e9da1828585
Benjamin Rose <benthere914@gmail.com>
2022-01-05 14:30:04 -0600

    home page styled and structured

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,9 +1,26 @@
+import { useState } from 'react'
 import './index.css'
+
 const Home = () => {
+    const [name, setName] = useState('')
+    const numbers = [];
+    const [buttonNumber, setButtonNumber] = useState(0)
+    for (let i = 1; i <= 5; i++){
+        numbers.push(i);
+    }
     return (
         <>
             <div className='homePage'>
-                <h1>The site is loaded. You can now edit the home page or add other components</h1>
+                <h1>Chat Application</h1>
+                <div className='roomForm'>
+                    <p>What is your name</p>
+                    <input value={name} onChange={(e) => {setName(e.target.value)}}/>
+                    <p>What room do you want to enter</p>
+                    <div className='rooms'>
+                        {numbers?.map((number) => <button onClick={() => {setButtonNumber(number)}} style={number === buttonNumber?{color: 'white', backgroundColor: 'black'}:null}>{`Room #${number}`}</button>)}
+                    </div>
+                    <button className='joinRoom'>Join Room</button>
+                </div>
             </div>
         </>
     )
